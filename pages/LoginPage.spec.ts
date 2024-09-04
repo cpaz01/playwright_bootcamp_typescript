7f1bfd6da2d55d5833f5f92ec1678a604d4eee1e
lirrera21 <lirrera@gmail.com>
2024-09-04 16:15:26 -0300

    TestW1W2

---
+++ pages/LoginPage.spec.ts
@@ -0,0 +1,24 @@
+import { Page } from '@playwright/test';
+const webside = ('https://opensource-demo.orangehrmlive.com/web/auth/login');
+
+export class LoginPage {
+  //creo como constantes
+  readonly page: Page;
+  readonly usernameInput: string = 'input[name="username"]';
+  readonly passwordInput: string = 'input[name="password"]';
+  readonly submitButton: string = 'button[type="submit"]';
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async navigate() {
+    await this.page.goto(webside);
+  }
+
+  async login(username: string, password: string) {
+    await this.page.fill(this.usernameInput, username);
+    await this.page.fill(this.passwordInput, password);
+    await this.page.click(this.submitButton);
+  }
+}
---
+++ tests/E2E.spec.ts
@@ -0,0 +1,54 @@
+import { test } from '@playwright/test';
+import { LoginPage } from '/LuciRepo/playwright_bootcamp_typescript-2/playwright_bootcamp_typescript/pages/LoginPage.spec';
+import { MyInfoPage } from '/LuciRepo/playwright_bootcamp_typescript-2/playwright_bootcamp_typescript/pages/MyInfoPage.spec';
+import data from '/LuciRepo/playwright_bootcamp_typescript-2/playwright_bootcamp_typescript/data/data.json';
+
+//import { beforeEach } from 'node:test';
+
+/*
+test.beforeEach(async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const myInfoPage = new MyInfoPage(page);
+})
+*/
+
+
+test('Login to the application', async ({ page }) => {
+   
+  const loginPage = new LoginPage(page);
+
+    await loginPage.navigate();
+
+    await page.waitForSelector(loginPage.submitButton, { state: 'visible' });
+    await page.screenshot({ path: './screenshots/LoginPage.png' });
+  
+    await loginPage.login(data.username, data.password);
+ 
+
+});
+
+test.describe('My Info Page Tests', () => {
+  test('Update Personal Details and Validate', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const myInfoPage = new MyInfoPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login(data.username, data.password);
+
+    await myInfoPage.navigateToMyInfo();
+    await myInfoPage.updatePersonalDetails(data.firstName, data.lastName);
+    await myInfoPage.assertPersonalDetails(data.firstName, data.lastName);
+  });
+
+  test('Add Emergency Contact and Validate', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const myInfoPage = new MyInfoPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login(data.username, data.password);
+
+    await myInfoPage.navigateToMyInfo();
+    await myInfoPage.addEmergencyContact(data.name, data.relationship, data.homeTelephone);
+    await myInfoPage.assertEmergencyContactAdded(data.name);
+  });
+});
